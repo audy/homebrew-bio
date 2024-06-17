04a4fef5cd45b76dc01bdd1ef14ef96935ec6627
Austin Richardson <accounts@agdr.org>
2024-06-16 23:52:59 -0700

    Add usearch12

--- Formula/usearch12.rb
+++ Formula/usearch12.rb
@@ -1,15 +1,15 @@
 class Usearch12 < Formula
-  desc "USEARCH offers search and clustering algorithms that are often orders of magnitude faster than BLAST"
+  desc "Search & clustering algorithms often orders of magnitude faster than BLAST"
   homepage "https://drive5.com/usearch"
   url "https://github.com/rcedgar/usearch12/archive/refs/tags/v12.0-beta1.tar.gz"
   sha256 "dbb06e4733801dab1c405616880261bd885ab171dfdb1d44e8ede48d739cdc43"
   license "GPL-3.0"
 
-  depends_on "cmake" => :build
   depends_on "ccache" => :build
+  depends_on "cmake" => :build
 
   def install
-    cd 'src' do
+    cd "src" do
       system "make"
     end
 
@@ -17,6 +17,6 @@ class Usearch12 < Formula
   end
 
   test do
-    system "usearch12"
+    system "#{bin}/usearch12"
   end
 end
